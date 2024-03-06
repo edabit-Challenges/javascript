52c7d1c4e679e67d4d9aa7cbb9f5123bddfee043
hamidehalaee <hamidehalaeeyasar@gmail.com>
2024-03-06 17:01:43 +0330

    solved

---
+++ medium/Find the nth Tetrahedral Number/Find the nth Tetrahedral Number.js
@@ -0,0 +1,10 @@
+//This function calculates the nth tetrahedral number
+// using the formula
+// (n * (n + 1) * (n + 2)) / 6 and returns the result.
+function tetra(n) {
+    return (n * (n + 1) * (n + 2)) / 6;
+  }
+
+
+
+
