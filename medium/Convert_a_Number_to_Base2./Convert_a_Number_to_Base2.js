523808e9bec3003386f669b586522ecde9b0ec4c
hamidehalaee <hamidehalaeeyasar@gmail.com>
2024-03-06 15:08:37 +0330

    solved

---
+++ medium/Convert_a_Number_to_Base2./Convert_a_Number_to_Base2.js
@@ -0,0 +1,22 @@
+function binary(decimal) {
+	if (decimal === 0) return "0";
+
+  let result = '';
+  let value = 1;
+  
+  // Find the leftmost bit that is greater than decimal
+  while (value <= decimal) {
+    value <<= 1;
+  }
+  
+  // Adjust value to the right
+  value >>= 1;
+
+  // Build the binary representation
+  while (value > 0) {
+    result += decimal & value ? '1' : '0';
+    value >>= 1;
+  }
+  
+  return result;
+}
